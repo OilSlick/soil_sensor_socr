f40069b8109dd6e71b2739dcd9268d5fd65a6ded
Michael <Michael@OilSlickCoffee.com>
2020-03-19 09:35:55 +0800

    Add millis timer

--- soil_sensor_socr.ino
+++ soil_sensor_socr.ino
@@ -21,13 +21,13 @@ byte data[12];
 byte addr[8];
 float celsius, fahrenheit;
 
-
 //For LoRa 
 #include <LoRa.h>                     //Needed for LoRa
 #define RFM95_SS 8                    //The CS pin (#8) does not have a pullup built in so be sure to set this pin HIGH when not using the radio!
 #define RFM95_RST 4
 #define RFM95_INT 3
 
+//For LoRa Transmissions
 byte localAddress = 06;               // address of this device 
 byte webGatewayAddress = 0x31;        // address of web gateway
 byte broadcastAddress = 0xFF;         // broadcast address
@@ -36,7 +36,10 @@ byte msgCount = 0;                    // count of outgoing messages
 byte fullpayload[12];                 // Full payload to be sent              
 int sizeofFullPayload;                // Size of full payload
 
+//for local device
 bool debug = 0;
+long previousMillis = 0;              //stores the last time data collected
+long intervalMinutes = 60000;        //Polling interval in minutes * 60 * 1000
 
 //For temp probes
 byte AddrTempProbeA[8] = {0x28, 0x1F, 0x2D, 0x45, 0x92, 0x5, 0x2, 0x30};
@@ -52,6 +55,7 @@ int ValueRawMoistureSensorB;          // Raw value returned from moisture sensor
 
 void setup(void) {
   if ( Serial ) Serial.begin(115200);
+  delay(2000);
   //LoRa.setPins(csPin, resetPin, irqPin);
   LoRa.setPins(RFM95_SS, RFM95_RST, RFM95_INT);
   //LoRa.setTxPower(17, PA_OUTPUT_PA_BOOST_PIN);
@@ -63,71 +67,76 @@ void setup(void) {
     if ( Serial ) Serial.print(".");
       delay(500);
   }
+  if ( Serial ) Serial.println("Setup done.Counting millis...");
 }
 
 void loop(void) {
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis > intervalMinutes)
+  {
+    previousMillis = currentMillis;
 
-  //scanOneWire();
-  //idOneWireDevices();
 
-  if ( Serial ) Serial.println("Temp Probe A");
-  pollTempProbe(AddrTempProbeA);
-  decodeProbeData();
-  memset(data, 0, sizeof(data)); //clear array
+    //scanOneWire();
+    //idOneWireDevices();
   
-  delay(5000);
+    if ( Serial ) Serial.println("Temp Probe A");
+    pollTempProbe(AddrTempProbeA);
+    decodeProbeData();
+    memset(data, 0, sizeof(data)); //clear array
+  
+    delay(5000);
 
-  if ( Serial )  
-  {
-    Serial.println(" ");
-    Serial.print("Moisture Probe A: ");
-  }
-  ValueRawMoistureSensorA = analogRead(moistureSensorA);
-  if ( Serial )
-  {
-    Serial.print(ValueRawMoistureSensorA);
-    Serial.print(" (raw) ");
-  }
-  ValueRawMoistureSensorA = map(ValueRawMoistureSensorA,1023,581,0,100); //map(value, fromLow, fromHigh, toLow, toHigh)
-  data[0] = byte(ValueRawMoistureSensorA);
-  if ( Serial )
-  {
-    Serial.print(ValueRawMoistureSensorA);
-    Serial.println("%");
-    Serial.println(" ");
-  }
+    if ( Serial )  
+    {
+      Serial.println(" ");
+      Serial.print("Moisture Probe A: ");
+    }
+    ValueRawMoistureSensorA = analogRead(moistureSensorA);
+    if ( Serial )
+    {
+      Serial.print(ValueRawMoistureSensorA);
+      Serial.print(" (raw) ");
+    }
+    ValueRawMoistureSensorA = map(ValueRawMoistureSensorA,1023,581,0,100); //map(value, fromLow, fromHigh, toLow, toHigh)
+    data[0] = byte(ValueRawMoistureSensorA);
+    if ( Serial )
+    {
+      Serial.print(ValueRawMoistureSensorA);
+      Serial.println("%");
+      Serial.println(" ");
+    }
   
-  broadcastdata(data, 1, AddrMoistProbeA);
-  memset(data, 0, sizeof(data)); //clear array
+    broadcastdata(data, 1, AddrMoistProbeA);
+    memset(data, 0, sizeof(data)); //clear array
   
-  if ( Serial )Serial.println("Temp Probe B");
-  pollTempProbe(AddrTempProbeB);
-  decodeProbeData();
-  memset(data, 0, sizeof(data)); //clear array
+    if ( Serial )Serial.println("Temp Probe B");
+    pollTempProbe(AddrTempProbeB);
+    decodeProbeData();
+    memset(data, 0, sizeof(data)); //clear array
 
-  if ( Serial )
-  {
-    Serial.println(" ");
-    Serial.print("Moisture Probe B: ");
-  }
-  ValueRawMoistureSensorB = analogRead(moistureSensorB);
-  if ( Serial )
-  {
-    Serial.print(ValueRawMoistureSensorB);
-    Serial.print(" (raw) ");
-  }
-  ValueRawMoistureSensorB = map(ValueRawMoistureSensorB,1023,581,0,100); //map(value, fromLow, fromHigh, toLow, toHigh)
-  data[0] = byte(ValueRawMoistureSensorB);
-  if ( Serial )
-  {
-    Serial.print(ValueRawMoistureSensorB);
-    Serial.println("%");
-    Serial.println(" ");
-  }
-  broadcastdata(data, 1, AddrMoistProbeB);
-  memset(data, 0, sizeof(data)); //clear array
+    if ( Serial )
+    {
+      Serial.println(" ");
+      Serial.print("Moisture Probe B: ");
+    }
+    ValueRawMoistureSensorB = analogRead(moistureSensorB);
+    if ( Serial )
+    {
+      Serial.print(ValueRawMoistureSensorB);
+      Serial.print(" (raw) ");
+    }
+    ValueRawMoistureSensorB = map(ValueRawMoistureSensorB,1023,581,0,100); //map(value, fromLow, fromHigh, toLow, toHigh)
+    data[0] = byte(ValueRawMoistureSensorB);
+    if ( Serial )
+    {
+      Serial.print(ValueRawMoistureSensorB);
+      Serial.println("%");
+      Serial.println(" ");
+    }
+    broadcastdata(data, 1, AddrMoistProbeB);
+    memset(data, 0, sizeof(data)); //clear array
   
-  //LoRa.sleep();
-  //delay(1000*60*30); //delay 30 minutes
-  delay(1000);
+    LoRa.sleep();
+   }
 }
